f82f46804486f5136e2f4d28d869e95e3f8df0c1
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-05 22:15:16 +0300

    add member variables and a public constructor in Ranger class

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,4 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Ranger {
+    private static int id;
+    private String name;
+    private int badge_number;
+    private int phone_number;
+    private String email;
+    private static ArrayList<Ranger> rangers = new ArrayList<>();
+
+    public Ranger(String name, int badge_number, int phone_number, String email){
+        this.name = name;
+        this.badge_number = badge_number;
+        this.phone_number = phone_number;
+        this.email = email;
+        rangers.add(this);
+        this.id = rangers.size();
+    }
 }
