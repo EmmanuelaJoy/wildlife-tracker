29c0497e6a8b5cedbb03d05884488082427ab4cc
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-05 22:25:35 +0300

    add a test that checks whether Ranger objects are instantiated correctly

--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,15 @@ public class RangerTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesRangerObjectsCorrectly_true() {
+        Ranger ranger = newRanger();
+        assertTrue(ranger instanceof Ranger);
+    }
+
+    private Ranger newRanger(){
+        return new Ranger("Emma", 23, 555, "joy@gmail.com");
     }
 }
