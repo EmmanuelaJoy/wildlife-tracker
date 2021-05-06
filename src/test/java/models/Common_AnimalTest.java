a0bd3cbcd7aef4d513663e4586567e08283baaaf
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 18:49:17 +0300

    add a test that ensures common animal objects are instantiated correctly

--- src/test/java/models/Common_AnimalTest.java
+++ src/test/java/models/Common_AnimalTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,15 @@ public class Common_AnimalTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesCommonAnimalObjectsCorrectly_true() {
+        Common_Animal common_animal = newCommonAnimal();
+        assertTrue(common_animal instanceof Common_Animal);
+    }
+
+    private Common_Animal newCommonAnimal(){
+        return new Common_Animal("common", "zebra", "young");
     }
 }
