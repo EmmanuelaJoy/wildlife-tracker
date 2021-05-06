a8f09d16fc3200ae0002862ea769d28576da2053
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 20:04:50 +0300

    add a test that ensures Location objects are instantiated correctly

--- src/test/java/models/LocationTest.java
+++ src/test/java/models/LocationTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,15 @@ public class LocationTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesLocationObjectsCorrectly_true() {
+        Location location = newLocation();
+        assertTrue(location instanceof Location);
+    }
+
+    private Location newLocation(){
+        return new Location("Zone A");
     }
 }
