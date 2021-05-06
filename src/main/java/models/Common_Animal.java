741aa19d38db744517aa2a9d9513ac7e56c30236
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 18:43:42 +0300

    add member variables and a public constructor to common_animal class

--- src/main/java/models/Common_Animal.java
+++ src/main/java/models/Common_Animal.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Common_Animal {
+    private int id;
+    public static String type="common";
+    private String name;
+    private String age;
+    private static ArrayList<Common_Animal> common_animals = new ArrayList<>();
+
+    public Common_Animal(String type, String name, String age) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        common_animals.add(this);
+        this.id = common_animals.size();
+    }
 }
