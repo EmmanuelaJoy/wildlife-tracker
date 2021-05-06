4185d9ba8dd22a5b0a2bbc8bfe3fc6ba30be8b15
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 19:20:28 +0300

    add a test that ensures Endangered Animal objects are instantiated correctly

--- src/test/java/models/Endangered_AnimalTest.java
+++ src/test/java/models/Endangered_AnimalTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,15 @@ public class Endangered_AnimalTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void endangeredAnimalObjectsInstantiateCorrectly_true() {
+        Endangered_Animal endangered_animal = newEndangeredAnimal();
+        assertTrue(endangered_animal instanceof Endangered_Animal);
+    }
+
+    private Endangered_Animal newEndangeredAnimal(){
+        return new Endangered_Animal("endangered", "rhino", "old", "okay");
     }
 }
