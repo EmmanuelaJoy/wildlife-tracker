50b2a7d07d19e1816e11d944a44d5888de920e1a
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 20:30:03 +0300

    add member variables and a public constructor to Sighting class

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,23 @@
 package models;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Sighting {
+    private static int id;
+    private int rangerID;
+    private int animalID;
+    private int locationID;
+    private Date date = new Date();
+    private Timestamp timestamp;
+    private static ArrayList<Sighting> sightings = new ArrayList<>();
+
+    public Sighting(int rangerID, int animalID, int locationID){
+        this.rangerID = rangerID;
+        this.animalID = animalID;
+        this.locationID = locationID;
+        this.timestamp = new Timestamp(date.getTime());
+        this.id = sightings.size();
+    }
 }
