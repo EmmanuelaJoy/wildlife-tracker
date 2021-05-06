b2131e7b97d92a4a15e512c04d32b36d03ebd791
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 19:36:41 +0300

    add member variables and a public constructor to the Location class

--- src/main/java/models/Location.java
+++ src/main/java/models/Location.java
@@ -1,5 +1,15 @@
 package models;
 
-public class Location {
+import java.util.ArrayList;
 
+public class Location {
+    private static int id;
+    private String name;
+    private static ArrayList<Location> locations = new ArrayList<>();
+
+    public Location(String name){
+        this.name=name;
+        locations.add(this);
+        this.id = locations.size();
+    }
 }
