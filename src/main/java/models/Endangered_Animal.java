26aa1d092c2d3c8e5fa52f9d7dac7d82de164ab8
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-06 19:13:00 +0300

    add member variables and a public constructor to Endangered Animal class

--- src/main/java/models/Endangered_Animal.java
+++ src/main/java/models/Endangered_Animal.java
@@ -1,5 +1,21 @@
 package models;
 
-public class Endangered_Animal {
+import java.util.ArrayList;
 
+public class Endangered_Animal {
+    private static int id;
+    public static String type="endangered";
+    private String name;
+    private String age;
+    private String health_status;
+    private static ArrayList<Endangered_Animal> endangered_animals = new ArrayList<>();
+
+    public Endangered_Animal(String type, String name, String age, String health_status){
+        this.type= type;
+        this.name = name;
+        this.age = age;
+        this.health_status = health_status;
+        endangered_animals.add(this);
+        this.id = endangered_animals.size();
+    }
 }
